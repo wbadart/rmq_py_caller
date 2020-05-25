096a67eee86ca65d83a285a30c365ff97e1337be
Will Badart <badart_william@bah.com>
2020-05-25 09:21:53 -0700

    It's working :)
    Added a bunch of explanatory comments; hopefully they're not too
    verbose. Also locked down the logging approach a little better. I
    normally don't like to do too much of that, but since we're juggling a
    couple threads + async, I figure best to leave the option in there to
    help end users see what's getting called when.

--- rmq_py_caller/__main__.py
+++ rmq_py_caller/__main__.py
@@ -1,15 +1,15 @@
 """rmq_pyctx_caller/__main__.py
 
-Load a context manager and repeatedly call the callable its `__enter__` method
-returns.
+Load a context manager and call the callable its `__enter__` method returns on
+lines of JSON from stdin.
 
 created: MAY 2020
 """
 
-import asyncio
 import json
+import logging
 import sys
-from concurrent.futures import ThreadPoolExecutor
+from argparse import ArgumentParser
 from contextlib import nullcontext
 from os import environ
 from queue import Queue
@@ -27,18 +27,53 @@ def is_context_manager(ctx):
 
 def main():
     """Setup PY_TARGET and call it on each line of JSON on stdin."""
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="-v to include info, -vv to include debugging",
+    )
+    args = parser.parse_args()
+    log = logging.getLogger(__name__)
+    log.setLevel([logging.WARN, logging.INFO, logging.DEBUG][args.verbose])
+
+    # If you're curious about the use exec/ eval here, please see the project
+    # wiki for discussion.
+    # pylint: disable=exec-used,eval-used
     exec(environ.get("PY_SETUP", ""), globals())
     ctx = eval(environ["PY_TARGET"])
+    # Wait until now to call basicConfig in case PY_SETUP wants to do it
+    logging.basicConfig()
+    log.debug("Got PY_TARGET: %s", ctx)
+
+    # Our worker will use ctx in a `with` statement shortly (for resource
+    # management). If PY_TARGET is just a regular function, wrap it in the
+    # nullcontext so we can pretend it's a context manager that yields the
+    # function (and in so doing, not have to invent specialized handling; keep
+    # it to one unified process).
     if not is_context_manager(ctx):
         ctx = nullcontext(ctx)
 
+    # `worker` will run in a different thread so that it can keep at it while
+    # we're blocked reading stdin. We'll send it inputs via a thread-safe
+    # stdlib queue.Queue.
     adapter = jq.compile(environ.get("ARG_ADAPTER", "[.]"))
+    log.debug("Using ARG_ADAPTER: %s", adapter.program_string)
     queue = Queue()
-    Thread(target=worker, args=(queue, ctx, adapter), daemon=True).start()
+    worker_args = queue, ctx, adapter, args.verbose
+    worker_thread = Thread(target=worker, args=worker_args)
+    worker_thread.start()
 
     for line in sys.stdin:
-        queue.put(line)
+        log.debug("Read stdin line: %s", line)
+        payload = json.loads(line)
+        queue.put(payload)
+    # When there's no more input, tell `worker` to shutdown by sending `None`
     queue.put(None)
+    worker_thread.join()
+    log.info("Goodbye from rmq_py_caller!")
 
 
 if __name__ == "__main__":
--- rmq_py_caller/worker.py
+++ rmq_py_caller/worker.py
@@ -1,34 +1,78 @@
+"""rmq_py_caller/worker.py
+
+This module exports the `worker` function. Calling `worker` will spin up an
+async program that will process JSON objects you send to its `inputs` queue.
+
+created: MAY 2020
+"""
+
 import asyncio
 import json
+import logging
 import sys
 from concurrent.futures import ThreadPoolExecutor
 
+__all__ = ["worker"]
 
-def worker(inputs, ctx, adapter):
+
+def worker(inputs, ctx, adapter, verbose):
     """
     Enter `ctx` and call the resulting function on each message on the queue
     `inputs`.
     """
+    log = logging.getLogger(__name__)
+    log.setLevel([logging.WARN, logging.INFO, logging.DEBUG][verbose])
 
     async def _main():
+        # _main is responsible for calling func. However, sometimes func might
+        # be a coroutine, and _main doesn't wan't to `await` the result before
+        # moving onto the next queue item. Instead, send results (via the
+        # `outbox` queue) to the _printer task, which will `await` and print
+        # the reult.
         outbox = asyncio.Queue()
         asyncio.create_task(_printer(outbox))
+
+        # Need a handle on the event loop for run_in_executor; see below
         loop = asyncio.get_running_loop()
-        with ctx as func, ThreadPoolExecutor() as pool:
+
+        # Enter ctx to perform any setup for func, and guarantee teardown
+        with ctx as func, ThreadPoolExecutor() as bg_thread:
             while True:
-                payload = await loop.run_in_executor(pool, inputs.get)
+                # `inputs` is a synchronous queue (stdlib queue.Queue) so .get
+                # would block the current thread. Running in a separate
+                # executor (bg_thread) allows this thread to stay unblocked, in
+                # turn allowing the loop to schedule other tasks while we await
+                # a payload
+                log.info("Waiting for payload...")
+                payload = await loop.run_in_executor(bg_thread, inputs.get)
+                log.debug("Got payload: %s", payload)
+
+                # rmq_py_caller.__main__.main will send us `None` when it's
+                # time to shutdown
                 if payload is None:
                     break
-                args = adapter.input(text=payload).first()
-                res = func(*args)
-                await outbox.put(res)
+
+                # Apply ARG_ADAPTER (which maps the input object to an array of
+                # args for PY_TARGET), call PY_TARGET, and send the result to
+                # the _printer
+                args = adapter.input(payload).first()
+                log.debug("ARG_ADAPTER resulted in: %s", args)
+                result = func(*args)
+                log.debug("PY_TARGET resulted in: %s", result)
+                await outbox.put((result, payload))
+
+            log.info("Goodbye from rmq_py_caller worker!")
 
     async def _printer(inbox):
+        # I just wait for _main to send me stuff to print. If what _main sends
+        # me is a coroutine object, I'll wait on that too before printing.
         while True:
-            res = await inbox.get()
-            if asyncio.iscoroutine(res):
-                res = await res
-            json.dump(res, fp=sys.stdout)
+            result, orig = await inbox.get()
+            if asyncio.iscoroutine(result):
+                log.info("_printer got a coroutine. Awaiting it...")
+                result = await result
+            log.info("Printing...")
+            json.dump({"result": result, "orig": orig}, fp=sys.stdout)
             print()
 
     asyncio.run(_main())
